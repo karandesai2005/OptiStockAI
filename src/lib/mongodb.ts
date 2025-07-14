5952e8ae942a721ed1f5e0a81728c0c1faad3657
Karan.Desai Btech2023 <karan.desai.btech2023@sitpune.edu.in>
2025-07-14 06:11:24 +0000

    bro wtf this is the connection string !! mongodb+srv://cruxal1611:Cruxal

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://cruxal1611:Cruxal1611@optistockai.miiiq5h.mongodb.net/?retryWrites=true&w=majority&appName=OptiStockAI";
 const options = {};
 
 let client;
